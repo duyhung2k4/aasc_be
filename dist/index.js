18fb36cde0d4d2670595d57f167dcbeca589f4f0
ndhung <nguyenduyhung04092004@gmail.com>
2024-08-17 02:55:26 +0700

    .

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const routers_1 = __importDefault(require("./routers"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
 app.use(routers_1.default);
